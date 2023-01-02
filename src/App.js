887068c7d80f2d9be0e7d540e901f266d2144a88
hdubey64 <hdubey64@gmail.com>
2023-01-02 16:06:35 +0530

    Header Imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-//react-router
-// import {
-//    BrowserRouter as Router,
-//    Switch,
-//    Routes,
-//    Route,
-//    Link,
-// } from "react-router-dom";
-// import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 import "firebase/auth";
 import Header from "./Layout/Header";
 
 function App() {
    return (
-      <div className="App">
+      <div>
          <Header />
       </div>
    );
