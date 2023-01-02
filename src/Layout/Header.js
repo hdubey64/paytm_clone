1a78f15a1ae8ecf7bc9c8c7992d699a2d35d0c49
hdubey64 <hdubey64@gmail.com>
2023-01-02 16:05:38 +0530

    Container Added

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import {
+   Container,
    Collapse,
    Navbar,
    NavbarToggler,
@@ -8,46 +9,51 @@ import {
    NavItem,
    NavLink,
    NavbarText,
+   Button,
 } from "reactstrap";
 import paytm_logo from "../Assets/Imegas/paytm_logo.png";
 import { Link } from "react-router-dom";
+import SignInButton from "../Components/SignInButton";
 
 const Header = () => {
    const [isOpen, setIsOpen] = useState(false);
    const toggle = () => setIsOpen(!isOpen);
 
    return (
-      <Navbar color="white" light expand="md">
-         <NavbarBrand>
-            <img src={paytm_logo} alt="Brand Pic" width={180} />
-         </NavbarBrand>
-         <NavbarToggler onClick={toggle} />
-         <Collapse isOpen={isOpen} navbar>
-            <Nav className="ms-auto">
-               <NavItem>
-                  <NavLink className="text-black fw-bold">
-                     Paytm for Consumer
-                  </NavLink>
-               </NavItem>
-               <NavItem>
-                  <NavLink className="text-black fw-bold">
-                     Paytm for Business
-                  </NavLink>
-               </NavItem>
-               <NavItem>
-                  <NavLink className="text-black fw-bold">
-                     Invester Relation
-                  </NavLink>
-               </NavItem>
-               <NavItem>
-                  <NavLink className="text-black fw-bold">Company </NavLink>
-               </NavItem>
-               <NavItem>
-                  <NavLink className="text-black fw-bold">Career</NavLink>
-               </NavItem>
-            </Nav>
-         </Collapse>
-      </Navbar>
+      <Container>
+         <Navbar color="white" light expand="md">
+            <NavbarBrand>
+               <img src={paytm_logo} alt="Brand Pic" width={180} />
+            </NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+               <Nav className="ms-auto">
+                  <NavItem>
+                     <NavLink className="text-black fw-bold">
+                        Paytm for Consumer
+                     </NavLink>
+                  </NavItem>
+                  <NavItem>
+                     <NavLink className="text-black fw-bold">
+                        Paytm for Business
+                     </NavLink>
+                  </NavItem>
+                  <NavItem>
+                     <NavLink className="text-black fw-bold">
+                        Invester Relation
+                     </NavLink>
+                  </NavItem>
+                  <NavItem>
+                     <NavLink className="text-black fw-bold">Company </NavLink>
+                  </NavItem>
+                  <NavItem>
+                     <NavLink className="text-black fw-bold">Career</NavLink>
+                  </NavItem>
+               </Nav>
+            </Collapse>{" "}
+            <SignInButton />
+         </Navbar>
+      </Container>
    );
 };
 
