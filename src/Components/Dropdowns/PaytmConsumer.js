ade2caf08db96176bcf99fec7eb3b8033336b112
hdubey64 <hdubey64@gmail.com>
2023-01-03 17:03:20 +0530

    PaytmConsumer Component Ready

---
+++ src/Components/Dropdowns/PaytmConsumer.js
@@ -0,0 +1,26 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import "./PaytmConsumer.css";
+
+function PaytmConsumer() {
+   return (
+      <Dropdown>
+         <Dropdown.Toggle className="consumer" id="dropdown-custom">
+            Paytm for Consumer
+         </Dropdown.Toggle>
+
+         <Dropdown.Menu>
+            <Dropdown.Item className="p-2" href="#/action-1">
+               Payments{" "}
+            </Dropdown.Item>
+            <Dropdown.Item className="p-2" href="#/action-2">
+               Ticket ooking
+            </Dropdown.Item>
+            <Dropdown.Item className="p-2" href="#/action-3">
+               Financial Services
+            </Dropdown.Item>
+         </Dropdown.Menu>
+      </Dropdown>
+   );
+}
+
+export default PaytmConsumer;
