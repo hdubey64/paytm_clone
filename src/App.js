bdc3688593c4b807b08fcbe430b1a43f3275becf
hdubey64 <hdubey64@gmail.com>
2023-01-03 17:04:31 +0530

    file Import

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 import "firebase/auth";
 import Header from "./Layout/Header";
 import { HeroSection } from "./Layout/HeroSection";
+import BillsSection from "./Layout/BillsSection";
 
 function App() {
    return (
       <div>
          <Header />
          <HeroSection />
+         <BillsSection />
       </div>
    );
 }
---
+++ src/Layout/BillsSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BillsSection = () => {
+   return (
+      <div>
+         <h1>Reacharge & pay Bills On Paytm</h1>
+      </div>
+   );
+};
+
+export default BillsSection;
