da430e80968e3948defeb608a79076df22e0b99a
hdubey64 <hdubey64@gmail.com>
2023-01-04 11:34:10 +0530

    BillSection Ready

--- src/Layout/BillsSection.js
+++ src/Layout/BillsSection.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Card from "../Components/Card";
+import "./BillsSection.css";
 
 const BillsSection = () => {
    return (
-      <div>
-         <h1>Reacharge & pay Bills On Paytm</h1>
+      <div className="bills">
+         <Container>
+            <h1>Reacharge & pay Bills On Paytm</h1>
+            <div className="billsCard">
+               <Card
+                  buttonText="Recharge Prepaid Mobile >"
+                  img="https://assetscdn1.paytm.com/images/catalog/view_item/733299/1626251017535.png"
+               />
+               <Card
+                  buttonText="Pay
+                  Electricity
+                  Bill >"
+                  img="https://assetscdn1.paytm.com/images/catalog/view_item/733308/1626251043534.png"
+               />
+               <Card
+                  buttonText="Recharge DTH Conection >"
+                  img="	https://assetscdn1.paytm.com/images/catalog/view_item/733311/1626251101045.png"
+               />
+               <Card
+                  buttonText="Book
+                  Gas
+                  Cylinder >"
+                  img="	https://assetscdn1.paytm.com/images/catalog/view_item/733307/1626251127863.png"
+               />
+               <Card
+                  buttonText="                 
+                  All
+                  Payment
+                  Servicese >"
+                  img="https://assetscdn1.paytm.com/images/catalog/view_item/757701/1626268580682.png"
+               />
+            </div>
+         </Container>
       </div>
    );
 };
