7d0f5b51d71c7faa4a33a9e2a56a86bfa895c62e
hdubey64 <hdubey64@gmail.com>
2023-01-11 18:15:20 +0530

    Tools Card Ready

--- src/Components/ToolsCard/index.js
+++ src/Components/ToolsCard/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Container } from "reactstrap";
+import { FaAngleRight } from "react-icons/fa";
+import "./style.css";
 
-const ToolsCard = () => {
-  return (
-    <div>ToolsCard</div>
-  )
-}
+const ToolsCard = ({
+   pic = "",
+   picWidth = "",
+   picHeight = "",
+   h3Line1 = "",
+   h3Line2 = "",
+   cardPara = "",
+   linkText = "",
+}) => {
+   return (
+      <div className=" toolsCard m-auto">
+         <Container>
+            <div className="toolsCardContent">
+               <img src={pic} alt="" width={picWidth} height={picHeight} />
 
-export default ToolsCard
+               <h3 className="toolsCardTag mb-4">
+                  <text>
+                     <br /> {h3Line1} <br /> {h3Line2} <br />
+                  </text>
+               </h3>
+               <p>{cardPara}</p>
+               <a href="#">{linkText} </a>
+            </div>
+         </Container>
+      </div>
+   );
+};
+
+export default ToolsCard;
