f852881d86df850830237117e6c3a5595f462a02
hdubey64 <hdubey64@gmail.com>
2023-01-16 19:37:23 +0530

    Accordian Parrent Camponent

---
+++ src/Components/AccordianTitle/index.jsx
@@ -0,0 +1,10 @@
+const AccordianTitle = ({ children }) => {
+   return (
+      <div className="accordionContent">
+         <p>{children}</p>
+         <div></div>
+      </div>
+   );
+};
+
+export default AccordianTitle;
