bf646c48f27f7fdc52fc588e9c2fbd3ca2db44d6
hdubey64 <hdubey64@gmail.com>
2023-01-17 22:16:18 +0530

    Button FW & TA Change

--- src/Components/SignInButton.js
+++ src/Components/SignInButton.js
@@ -5,8 +5,8 @@ import "./SignInButton.css";
 
 const SignInButton = () => {
    return (
-      <Button className="vishal ms-5">
-         Sign In <img src={Profile} alt="" />
+      <Button className="vishal  ps-4 py-0 pe-0 text-center fw-bold ">
+         Sign In <img src={Profile} alt="" className="ps-2 pe-0" />
       </Button>
    );
 };
