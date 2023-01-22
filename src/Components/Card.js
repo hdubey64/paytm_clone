77b91dfa555df5f20a6d1089f1e9feb96f5e45ce
hdubey64 <hdubey64@gmail.com>
2023-01-22 12:18:02 +0530

    Media Query Done

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,24 +2,31 @@ import React from "react";
 import "./Card.css";
 
 const Card = ({
-   buttonText = "My Button",
+   buttonText = { lineOne: "", lineTwo: "", lineThree: "" },
    img = "",
    className = "lightBlue",
+   btnClassName = "",
 }) => {
    return (
-      <div
-         className={`${className} card cardColor m-auto`}
-         style={{ width: " 166px", height: "186px" }}
-      >
-         <img
-            src={img}
-            className="card-img-top m-2"
-            style={{ width: 64, height: 64 }}
-            alt=""
-         />
-         <div className="card-body pe-5">
-            <a href="#" className={`${className} cardBtn`}>
-               {buttonText}
+      <div className={`${className} card cardColor`}>
+         <div className="imgAlign">
+            <img
+               src={img}
+               className="card-img-top"
+               style={{ width: 64, height: 64 }}
+               alt=""
+            />
+         </div>
+
+         <div className="card-body cardLink ">
+            <a href="#" className={`${btnClassName} cardBtn`}>
+               {buttonText.lineOne}
+            </a>
+            <a href="#" className={`${btnClassName} cardBtn`}>
+               {buttonText.lineTwo}
+            </a>
+            <a href="#" className={`${btnClassName} cardBtn`}>
+               {buttonText.lineThree}
             </a>
          </div>
       </div>
