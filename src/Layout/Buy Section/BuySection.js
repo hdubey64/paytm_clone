e943e27dcc86a2c1e9e00e41c6e73277cdea7b9f
hdubey64 <hdubey64@gmail.com>
2023-01-22 15:07:25 +0530

    cardData Added

--- src/Layout/Buy Section/BuySection.js
+++ src/Layout/Buy Section/BuySection.js
@@ -3,49 +3,76 @@ import { Container } from "reactstrap";
 import Card from "../../Components/Card";
 import "./BuySection.css";
 
+const cardData = [
+   {
+      buttonText: {
+         lineOne: "Movie",
+         lineTwo: "Tickets >",
+      },
+      img: "	https://assetscdn1.paytm.com/images/catalog/view_item/733295/1626259710574.png",
+   },
+   {
+      buttonText: {
+         lineOne: "Flight",
+         lineTwo: "Tickets >",
+      },
+      img: "https://assetscdn1.paytm.com/images/catalog/view_item/733296/1626259884425.png",
+   },
+   {
+      buttonText: {
+         lineOne: "Bus",
+         lineTwo: "Tickets >",
+      },
+      img: "	https://assetscdn1.paytm.com/images/catalog/view_item/729996/1626260477699.png",
+   },
+   {
+      buttonText: {
+         lineOne: "Train",
+         lineTwo: "Tickets >",
+      },
+      img: "	https://assetscdn1.paytm.com/images/catalog/view_item/729996/1626260477699.png",
+   },
+   {
+      buttonText: {
+         lineOne: "Buy",
+         lineTwo: "Insurance >",
+      },
+      img: "https://assetscdn1.paytm.com/images/catalog/view_item/729998/1666185237748.png",
+   },
+
+   {
+      buttonText: {
+         lineOne: "International",
+         lineTwo: "Flights >",
+      },
+      img: "	https://assetscdn1.paytm.com/images/catalog/view_item/729999/1626259968563.png",
+   },
+
+   {
+      buttonText: "Invest ",
+      buttonText: {
+         lineOne: "Invest",
+         lineTwo: "In Stock >",
+      },
+      img: "https://assetscdn1.paytm.com/images/catalog/view_item/730001/1626450848003.png",
+   },
+];
+
 const BuySection = () => {
    return (
       <div className="buy">
-         <Container>
+         <Container className="buyContent">
             <h1>Book & Buy on Paytm.</h1>
             <div className="buyCard">
-               <Card
-                  buttonText="Movie Tickets  >"
-                  img="	https://assetscdn1.paytm.com/images/catalog/view_item/733295/1626259710574.png"
-                  className="darkBlue"
-               />
-               <Card
-                  buttonText="Flight Tickets >"
-                  img="https://assetscdn1.paytm.com/images/catalog/view_item/733296/1626259884425.png"
-                  className="darkBlue"
-               />
-               <Card
-                  buttonText="Bus Tickets >"
-                  img="	https://assetscdn1.paytm.com/images/catalog/view_item/729996/1626260477699.png"
-                  className="darkBlue"
-               />
-               <Card
-                  buttonText="Train Tickets >"
-                  img="	https://assetscdn1.paytm.com/images/catalog/view_item/729996/1626260477699.png"
-                  className="darkBlue"
-               />
-               <Card
-                  buttonText="                 
-                 Buy Insurance >"
-                  img="https://assetscdn1.paytm.com/images/catalog/view_item/729998/1666185237748.png"
-                  className="darkBlue"
-               />
-               <Card
-                  buttonText="                 
-                  International Flights >"
-                  img="	https://assetscdn1.paytm.com/images/catalog/view_item/729999/1626259968563.png"
-                  className="darkBlue"
-               />
-               <Card
-                  buttonText="Invest In Stock >"
-                  img="https://assetscdn1.paytm.com/images/catalog/view_item/730001/1626450848003.png"
-                  className="darkBlue"
-               />
+               {cardData.map((item) => (
+                  <Card
+                     key={item.img}
+                     buttonText={item.buttonText}
+                     img={item.img}
+                     className="darkBlueColor"
+                     btnClassName="card-btn2"
+                  />
+               ))}
             </div>
          </Container>
       </div>
