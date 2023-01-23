3af2b6571906eff08058824132858fb796854ab2
hdubey64 <hdubey64@gmail.com>
2023-01-23 16:13:14 +0530

    Media Query Done

--- src/Layout/CardSection/index.js
+++ src/Layout/CardSection/index.js
@@ -4,50 +4,62 @@ import "./CardSection.css";
 
 const CardSection = () => {
    return (
-      <div className="cardSection">
+      <div className="unlimitedCardSection">
          <Container>
-            <Row className="cardContent">
-               <Col md={12} lg={6} className="card innerContent">
-                  <div>
-                     <img
-                        src="	https://assetscdn1.paytm.com/images/catalog/view_item/853975/1640242865113.png"
-                        alt=""
-                        width={185}
-                     />
-                     <h1>Unlimited Cashback Every time.</h1>
-                     <p>
-                        Paytm HDFC Bank Select Credit Card. A card with assured
-                        Cashback and incredible offers.
-                     </p>
-                  </div>
-                  <div className=" cardBanner">
-                     <img
-                        src="https://assetscdn1.paytm.com/images/catalog/view_item/853975/1640241561388.png"
-                        alt=""
-                     />
-                  </div>
-               </Col>
-               <Col md={12} lg={6} className="card innerContent ">
-                  <div>
-                     <img
-                        src="	https://assetscdn1.paytm.com/images/catalog/view_item/853976/1640242163727.png"
-                        alt=""
-                        width={185}
-                     />
-                     <h1>India’s Most Sincere Credit Card</h1>
-                     <p>
-                        Paytm SBI Card SELECT - With Intelligent Features &
-                        Great Rewards that Never Expire
-                     </p>
-                  </div>
-                  <div className=" cardBanner">
-                     <img
-                        src="	https://assetscdn1.paytm.com/images/catalog/view_item/853976/1626079147084.png"
-                        alt=""
-                     />
-                  </div>
-               </Col>
-            </Row>
+            <div className="unlimitedCardContent">
+               <Row>
+                  <Col md={12} lg={6}>
+                     <div className=" hdfcBank unlimitedColContent">
+                        <div className=" unlimitedCardInnerContent">
+                           <img
+                              src="	https://assetscdn1.paytm.com/images/catalog/view_item/853975/1640242865113.png"
+                              alt=""
+                              width={185}
+                              height={77}
+                           />
+                           <h1>Unlimited Cashback Every time.</h1>
+                           <p>
+                              Paytm HDFC Bank Select Credit Card. A card with
+                              assured Cashback and incredible offers.
+                           </p>
+                        </div>
+
+                        <div className=" debitCardBanner">
+                           <img
+                              src="https://assetscdn1.paytm.com/images/catalog/view_item/853975/1640241561388.png"
+                              alt=""
+                           />
+                        </div>
+                     </div>
+                  </Col>
+                  <Col md={12} lg={6}>
+                     <div className=" sbiBank unlimitedColContent">
+                        <div className="  unlimitedCardInnerContent ">
+                           <img
+                              src="	https://assetscdn1.paytm.com/images/catalog/view_item/853976/1640242163727.png"
+                              alt=""
+                              width={196}
+                              height={20}
+                              className="sbiIMgMargin"
+                           />
+                           <h1>India’s Most Sincere Credit Card</h1>
+                           <p>
+                              Paytm SBI Card SELECT - With Intelligent Features
+                              & Great Rewards that Never Expire
+                           </p>
+                        </div>
+
+                        <div className=" debitCardBanner">
+                           <img
+                              src="	https://assetscdn1.paytm.com/images/catalog/view_item/853976/1626079147084.png"
+                              alt=""
+                              className="bannerTopMargin"
+                           />
+                        </div>
+                     </div>
+                  </Col>
+               </Row>
+            </div>
          </Container>
       </div>
    );
