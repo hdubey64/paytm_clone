0c918937355aaa5a761c2bbe80cc467f1190727d
hdubey64 <hdubey64@gmail.com>
2023-01-23 19:01:58 +0530

    Media Query Done

--- src/Layout/PaytmMoney/index.js
+++ src/Layout/PaytmMoney/index.js
@@ -10,32 +10,37 @@ const PaytmMoney = () => {
             <div className="paytmMoneyContent">
                <Row>
                   <Col md={12} lg={6}>
-                     <div className="paytmMoneyInnerContent bannerPaytmMoneyInnerContent">
-                        <img
-                           src="https://assetscdn1.paytm.com/images/catalog/view_item/788781/1626077377376.png"
-                           alt=""
-                        />
+                     <div className="paytmMoneyColContent">
+                        <div className="paytmMoneyInnerContent bannerPaytmMoneyInnerContent">
+                           <img
+                              src="https://assetscdn1.paytm.com/images/catalog/view_item/788781/1626077377376.png"
+                              alt=""
+                           />
+                        </div>
                      </div>
                   </Col>
                   <Col md={12} lg={6}>
-                     <div className="paytmMoneyInnerContent">
-                        <div>
-                           <img
-                              src="https://assetscdn1.paytm.com/images/catalog/view/308774/1617696247991.png
+                     <div className="paytmMoneyColContent">
+                        <div className="paytmMoneyInnerContent">
+                           <div>
+                              <img
+                                 src="https://assetscdn1.paytm.com/images/catalog/view/308774/1617696247991.png
                   "
-                              alt=""
-                              width={145}
-                           />
-                        </div>
+                                 alt=""
+                                 width={145}
+                              />
+                           </div>
 
-                        <h1>Build Long-term Wealth & Achieve your Goals.</h1>
-                        <p>
-                           Investing on Paytm Money is transparent, low-cost and
-                           commission-free. Buy stocks & mutual funds that can
-                           help you create wealth & realise your dreams.
-                        </p>
-                        <div>
-                           <LearnMoreButton buttonText="Learn More" />
+                           <h1>Build Long-term Wealth & Achieve your Goals.</h1>
+                           <p>
+                              Investing on Paytm Money is transparent, low-cost
+                              and commission-free. Buy stocks & mutual funds
+                              that can help you create wealth & realise your
+                              dreams.
+                           </p>
+                           <div className="paytmMoneyButton">
+                              <LearnMoreButton buttonText="Learn More" />
+                           </div>
                         </div>
                      </div>
                   </Col>
